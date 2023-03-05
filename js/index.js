208f4f82da60111a7a83b60574925aaba11ffc2e
carolPortela <carol10.portela12@gmail.com>
2023-03-05 19:04:06 -0300

    apareceu as mensagens

--- js/index.js
+++ js/index.js
@@ -8,16 +8,14 @@ const criarCard = (contato, indice) => {
     conversa.classList.add('container-conversas')
 
     conversa.addEventListener('click', (event) => {
-       // carregarMensagens(conversas)
-
-      
-        console.log(contatos[indice])
-
+        var container = document.getElementById('container-chat')
+        container.appendChild(criarMensagem(indice))
+        container.appendChild(criarHeader(indice))
     })
 
     const foto = document.createElement('img')
     foto.classList.add('img-chat')
-    foto.src = `./imagens/contact1.png`
+    foto.src = `../img/${contato.image}`
 
     const informacao = document.createElement('div')
     informacao.classList.add('container-info')
@@ -37,65 +35,96 @@ const criarCard = (contato, indice) => {
 
 }
 
-const criarConversa = (message) => {
+const criarHeader = (indice) => {
 
-    const mensagem = document.createElement('div')
-    mensagem.classList.add('container-mensagens-direita')
+    const header = document.createElement('div')
+    header.classList.add('header-direito');
 
-    const caixaMinha = document.createElement('div')
-    caixaMinha.classList.add('caixa-mensagens-minha')
+    const containerHeader = document.createElement('div')
+    containerHeader.classList.add('container-header');
 
-    const caixaSua = document.createElement('div')
-    caixaSua.classList.add('caixa-mensagens-sua')
+    const imagemPerfil = document.createElement('img')
+    imagemPerfil.classList.add('img-perfil');
+    imagemPerfil.src = `../img/${contatos[indice].image}`
+
+    const containerPerfil = document.createElement('div')
+    containerPerfil.classList.add('container-perfil')
+
+    const infoNome = document.createElement('span')
+    infoNome.classList.add('info-nome')
+    infoNome.textContent = contatos[indice].name
+
+    const infoConversa = document.createElement('span')
+    infoConversa.classList.add('info-conversa')
+    infoConversa.textContent = 'online'
+
+    header.append(containerHeader)
+
+    containerHeader.append(imagemPerfil, containerPerfil)
+
+    containerPerfil.append(infoNome, infoConversa)
+
+    return header
+
+}
+
+const criarMensagem = (indice) => {
+
+    const containerMensagensDireita = document.createElement('div')
+    containerMensagensDireita.classList.add('container-mensagens-direita');
+
+    const caixaMensagensMinha = document.createElement('div')
+    caixaMensagensMinha.classList.add('caixa-mensagens-minha')
+
+    const caixaMensagensSua = document.createElement('div')
+    caixaMensagensSua.classList.add('caixa-mensagens-sua')
 
     const msgMinha = document.createElement('p')
     msgMinha.classList.add('msg-minha')
-    msgMinha.textContent = message.messages
 
     const horaMinha = document.createElement('span')
     horaMinha.classList.add('hora-minha')
-    horaMinha.textContent = message.messages
 
     const msgSua = document.createElement('p')
     msgSua.classList.add('msg-sua')
-    msgSua.textContent = message.messages.content
 
     const horaSua = document.createElement('span')
     horaSua.classList.add('hora-sua')
-    horaSua.textContent = message.messages.time
 
-    mensagem.append(msgMinha, horaMinha, msgSua, horaSua)
+    contatos[indice].messages.forEach((mensagem) => {
+        if (mensagem.sender == 'me') {
 
-    return mensagem
-    
+            msgMinha.classList.add('msg-minha')
+            msgMinha.textContent = mensagem.content
+
+            horaMinha.classList.add('hora-minha')
+            horaMinha.textContent = mensagem.time
+
+        } else if (mensagem.sender == contatos[indice].name) {
+
+            msgSua.classList.add('msg-sua')
+            msgSua.textContent = mensagem.content
+
+            horaSua.classList.add('hora-sua')
+            horaSua.textContent = mensagem.time
+
+        }
+    })
+
+    containerMensagensDireita.append(caixaMensagensMinha, caixaMensagensSua)
+
+    caixaMensagensMinha.append(msgMinha, horaMinha)
+
+    caixaMensagensSua.append(msgSua, horaSua)
+
+    return containerMensagensDireita
 
 }
 
 const carregarContatos = () => {
-    const container = document.getElementById('container')
+    const container = document.getElementById('container-mensagens')
     const contatosMensagens = contatos.map(criarCard)
     container.replaceChildren(...contatosMensagens)
 }
 
 carregarContatos()
-
-
-const carregarMensagens = () => {
-    const mensagem = document.getElementById('mensagem')
-    const conversas = contatos.map(criarConversa)
-    mensagem.replaceChildren(...conversas)
-   
-}
-var div = document.getElementById('container-mensagens');
-
-div.addEventListener('click', function() {
-
-    var container = document.getElementById('containerD');
-
-    container.classList.toggle('hide');
-
-});
-//console.log(carregarMensagens);
-
-carregarMensagens()
-
